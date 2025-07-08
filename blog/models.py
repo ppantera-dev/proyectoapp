987b2bfd834f247ff26b6fb4271ddeec88190af1
ppantera-dev <pabloezequielval@gmail.com>
2025-07-08 19:39:00 -0300

    Create models.py

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
