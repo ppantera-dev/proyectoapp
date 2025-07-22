550635083f50d16f30853cff90051ef7d871716d
ppantera-dev <pabloezequielval@gmail.com>
2025-07-22 20:55:09 -0300

    urls.py

--- mi_blog/urls.py
+++ mi_blog/urls.py
@@ -1,14 +1,19 @@
-# proyectoapp/mi_blog/urls.py (Archivo URL principal del proyecto)
-
-"""
-URL configuration for mi_blog project.
-
-The `urlpatterns` list routes URLs to views.
-"""
 from django.contrib import admin
-from django.urls import path, include # <--- Asegúrate de que 'include' esté aquí
+from django.urls import path, include
+from django.conf import settings # ¡IMPORTAR ESTA LÍNEA!
+from django.conf.urls.static import static # ¡IMPORTAR ESTA OTRA LÍNEA!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', include('blog.urls')), # <--- ¡SOLO ESTA LÍNEA DEBE IR AQUÍ PARA TU BLOG!
+    path('blog/', include('blog.urls')), # Esta línea ya la tienes
+    path('ckeditor/', include('ckeditor_uploader.urls')), # ¡AGREGAR ESTA LÍNEA!
+    path('', include('blog.urls')), # ¡AGREGAR ESTA LÍNEA! Esto hará que la URL "/" sea manejada por blog.urls
+    # Las URLs para las apps 'accounts' y 'messages_app' las agregaremos más adelante cuando las creemos.
+    # path('accounts/', include('accounts.urls')),
+    # path('messages/', include('messages_app.urls')),
 ]
+
+# ¡AGREGAR ESTE BLOQUE COMPLETO AL FINAL DEL ARCHIVO!
+# Configuración para servir archivos de MEDIA en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
