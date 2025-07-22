4e9df0bfcffc90ce408b352f7b17ead7da2fe9ed
ppantera-dev <pabloezequielval@gmail.com>
2025-07-22 20:10:12 -0300

    urls.py

--- mi_blog/urls.py
+++ mi_blog/urls.py
@@ -1,11 +1,13 @@
+# proyectoapp/mi_blog/urls.py
 """
 URL configuration for mi_blog project.
 
 The `urlpatterns` list routes URLs to views.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # <--- Asegúrate de importar 'include' aquí
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls')), # <--- ¡Esta es la línea que debes agregar!
 ]
