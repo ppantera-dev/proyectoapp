388b9ec4e3f66c5233acc999029160dfa3896112
ppantera-dev <pabloezequielval@gmail.com>
2025-07-22 21:01:28 -0300

    views.py

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,23 @@
+# proyectoapp/blog/views.py
 from django.shortcuts import render
-from .models import Post
+# from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+# from django.urls import reverse_lazy
+# from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+# from django.contrib.auth.decorators import login_required
+# from django.contrib import messages
 
-def post_list(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/post_list.html', {'posts': posts})
+from .models import Article # Necesitamos importar el modelo Article para 'home'
+# from .forms import ArticleForm
+
+# --- Vistas Generales ---
+
+def home(request):
+    """Vista de inicio/home. Muestra los 5 artículos más recientes."""
+    recent_articles = Article.objects.all().order_by('-published_date')[:5]
+    return render(request, 'blog/home.html', {'recent_articles': recent_articles})
+
+def about(request):
+    """Vista 'Acerca de mí'."""
+    return render(request, 'blog/about.html')
+
+# --- (Las vistas para los Articles y la Zona Restringida se agregarán más adelante) ---
